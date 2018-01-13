49f9d09b5d4ac6b32626f15d0bd1c22a092c1fd7
Pzqqt <821026875@qq.com>
2018-01-14 03:09:03 +0800

    Integrate Sourceforge check method to reduce the code

--- check_update.py
+++ check_update.py
@@ -4,6 +4,45 @@
 import json, time, re
 from tools import *
 
+def sf_check(build_info, bsObj, fast_flag, cl_flag = False, skip = 0):
+	if fast_flag:
+		try:
+			nb = bsObj.find("channel").find_all("item")
+			nb1 = nb[0]
+			if cl_flag:
+				nb2 = nb[1]
+				if nb2.find("files:extra-info").get_text() != "text":
+					nb1, nb2 = nb2, nb1
+				build_info['update_log'] = nb2.find("guid").get_text()
+			build_info['flink'] = nb1.find("guid").get_text()
+			fversion = build_info['flink'].split("/")[-2]
+			build_info['fdate'] = nb1.find("pubdate").get_text()
+			build_info['fmd5'] = nb1.find("media:hash").get_text()
+		except:
+			return None, None
+	else:
+		try:
+			nb = bsObj.find("table",{"id":"files_list"})\
+				.find_all("tbody")[0].find_all("tr")
+			nb1 = nb[skip]
+			if cl_flag:
+				nb2 = nb[skip + 1]
+				if nb1["title"].split(".")[-1] != "zip":
+					nb1, nb2 = nb2, nb1
+				build_info['update_log'] = \
+					nb2.find("th").find("a")["href"]
+			nb3 = json.loads(bsObj.find_all("script")[-1]\
+				.get_text().split(" = ",1)[-1].split(";",1)[0])
+			fversion = nb1["title"]
+			build_info['fmd5'] = nb3[fversion]["md5"]
+			build_info['fsha1'] = nb3[fversion]["sha1"]
+			build_info['fdate'] = nb1.find("td").find("abbr")["title"]
+			build_info['flink'] = nb1.find("th").find("a")["href"]
+			build_info['fsize'] = nb1.find_all("td")[1].get_text()
+		except:
+			return None, None
+	return fversion, build_info
+
 def aex(fast_flag, bs4_parser):
 	name = "aex"
 	build_info = {}
@@ -114,31 +153,10 @@ def aoscp(fast_flag, bs4_parser):
 	bsObj = get_bs(ual, bs4_parser)
 	if not bsObj:
 		return open_failed(name)
-	if fast_flag:
-		try:
-			nb = bsObj.find("channel").find_all("item")[0]
-			build_info['flink'] = nb.find("guid").get_text()
-			fversion = build_info['flink'].split("/")[-2]
-			build_info['fdate'] = nb.find("pubdate").get_text()
-			build_info['fmd5'] = nb.find("media:hash").get_text()
-		except:
-			return None
-	else:
-		try:
-			nb = \
-				bsObj.find("table",{"id":"files_list"})\
-				.find_all("tbody")[0].find_all("tr")[1]
-			nb2 = \
-				json.loads(bsObj.find_all("script")[-1]\
-				.get_text().split(" = ",1)[-1].split(";",1)[0])
-			fversion = nb["title"]
-			build_info['fmd5'] = nb2[fversion]["md5"]
-			build_info['fsha1'] = nb2[fversion]["sha1"]
-			build_info['fdate'] = nb.find("td").find("abbr")["title"]
-			build_info['flink'] = nb.find("th").find("a")["href"]
-			build_info['fsize'] = nb.find_all("td")[1].get_text()
-		except:
-			return analyze_failed(name)
+	fversion, build_info = \
+		sf_check(build_info, bsObj, fast_flag, skip = 1)
+	if fversion == None:
+		return analyze_failed(name)
 	return out_put(fast_flag, name, fversion, build_info)
 
 def aosip(fast_flag, bs4_parser):
@@ -204,29 +222,10 @@ def cardinal(fast_flag, bs4_parser):
 	bsObj = get_bs(ual, bs4_parser)
 	if not bsObj:
 		return open_failed(name)
-	if fast_flag:
-		try:
-			nb = bsObj.find("channel").find_all("item")[0]
-			build_info['flink'] = nb.find("guid").get_text()
-			fversion = build_info['flink'].split("/")[-2]
-			build_info['fdate'] = nb.find("pubdate").get_text()
-			build_info['fmd5'] = nb.find("media:hash").get_text()
-		except:
-			return None
-	else:
-		try:
-			nb = bsObj.find("table",{"id":"files_list"})\
-				.find_all("tbody")[0].find_all("tr")[1]
-			nb2 = json.loads(bsObj.find_all("script")[-1]\
-				.get_text().split(" = ",1)[-1].split(";",1)[0])
-			fversion = nb["title"]
-			build_info['fmd5'] = nb2[fversion]["md5"]
-			build_info['fsha1'] = nb2[fversion]["sha1"]
-			build_info['fdate'] = nb.find("td").find("abbr")["title"]
-			build_info['flink'] = nb.find("th").find("a")["href"]
-			build_info['fsize'] = nb.find_all("td")[1].get_text()
-		except:
-			return analyze_failed(name)
+	fversion, build_info = \
+		sf_check(build_info, bsObj, fast_flag, skip = 1)
+	if fversion == None:
+		return analyze_failed(name)
 	return out_put(fast_flag, name, fversion, build_info)
 
 def cosmicos(fast_flag, bs4_parser):
@@ -240,29 +239,9 @@ def cosmicos(fast_flag, bs4_parser):
 		bsObj = get_bs(ual, bs4_parser)
 	if not bsObj:
 		return open_failed(name)
-	if fast_flag:
-		try:
-			nb = bsObj.find("channel").find_all("item")[0]
-			build_info['flink'] = nb.find("guid").get_text()
-			fversion = build_info['flink'].split("/")[-2]
-			build_info['fdate'] = nb.find("pubdate").get_text()
-			build_info['fmd5'] = nb.find("media:hash").get_text()
-		except:
-			return None
-	else:
-		try:
-			nb = bsObj.find("table",{"id":"files_list"})\
-				.find_all("tbody")[0].find_all("tr")[0]
-			nb2 = json.loads(bsObj.find_all("script")[-1]\
-				.get_text().split(" = ",1)[-1].split(";",1)[0])
-			fversion = nb["title"]
-			build_info['fmd5'] = nb2[fversion]["md5"]
-			build_info['fsha1'] = nb2[fversion]["sha1"]
-			build_info['fdate'] = nb.find("td").find("abbr")["title"]
-			build_info['flink'] = nb.find("th").find("a")["href"]
-			build_info['fsize'] = nb.find_all("td")[1].get_text()
-		except:
-			return analyze_failed(name)
+	fversion, build_info = sf_check(build_info, bsObj, fast_flag)
+	if fversion == None:
+		return analyze_failed(name)
 	return out_put(fast_flag, name, fversion, build_info)
 
 def dotos(fast_flag, bs4_parser):
@@ -273,29 +252,9 @@ def dotos(fast_flag, bs4_parser):
 	bsObj = get_bs(ual, bs4_parser)
 	if not bsObj:
 		return open_failed(name)
-	if fast_flag:
-		try:
-			nb = bsObj.find("channel").find_all("item")[0]
-			build_info['flink'] = nb.find("guid").get_text()
-			fversion = build_info['flink'].split("/")[-2]
-			build_info['fdate'] = nb.find("pubdate").get_text()
-			build_info['fmd5'] = nb.find("media:hash").get_text()
-		except:
-			return None
-	else:
-		try:
-			nb = bsObj.find("table",{"id":"files_list"})\
-				.find_all("tbody")[0].find_all("tr")[0]
-			nb2 = json.loads(bsObj.find_all("script")[-1]\
-				.get_text().split(" = ",1)[-1].split(";",1)[0])
-			fversion = nb["title"]
-			build_info['fmd5'] = nb2[fversion]["md5"]
-			build_info['fsha1'] = nb2[fversion]["sha1"]
-			build_info['fdate'] = nb.find("td").find("abbr")["title"]
-			build_info['flink'] = nb.find("th").find("a")["href"]
-			build_info['fsize'] = nb.find_all("td")[1].get_text()
-		except:
-			return analyze_failed(name)
+	fversion, build_info = sf_check(build_info, bsObj, fast_flag)
+	if fversion == None:
+		return analyze_failed(name)
 	return out_put(fast_flag, name, fversion, build_info)
 
 def flyme(fast_flag, bs4_parser):
@@ -656,43 +615,14 @@ def nos_o(fast_flag, bs4_parser):
 	bsObj = get_bs(ual, bs4_parser)
 	if not bsObj:
 		return open_failed(name)
-	if fast_flag:
-		try:
-			nb = bsObj.find("channel").find_all("item")
-			nb1 = nb[0]; nb2 = nb[1]
-			if nb2.find("files:extra-info").get_text() != "text":
-				nb1, nb2 = nb2, nb1
-			build_info['update_log'] = nb2.find("guid").get_text()
-			flink1 = nb1.find("guid").get_text()
-			fversion = flink1.split("/")[-2]
-			build_info['fdate'] = nb1.find("pubdate").get_text()
-			build_info['fmd5'] = nb1.find("media:hash").get_text()
-		except:
-			return None
-	else:
-		try:
-			nb = bsObj.find("table",{"id":"files_list"})\
-				.find_all("tbody")[0].find_all("tr")[1]
-			nb2 = bsObj.find("table",{"id":"files_list"})\
-				.find_all("tbody")[0].find_all("tr")[0]
-			nb3 = json.loads(bsObj.find_all("script")[-1]\
-				.get_text().split(" = ",1)[-1].split(";",1)[0])
-			if nb["title"].split(".")[-1] != "zip":
-				nb, nb2 = nb2, nb
-			build_info['update_log'] = nb2.find("th").find("a")["href"]
-			fversion = nb["title"]
-			flink1 = nb.find("th").find("a")["href"]
-			build_info['fmd5'] = nb3[fversion]["md5"]
-			build_info['fsha1'] = nb3[fversion]["sha1"]
-			build_info['fdate'] = nb.find("td").find("abbr")["title"]
-			build_info['fsize'] = nb.find_all("td")[1].get_text()
-		except:
-			return analyze_failed(name)
+	fversion, build_info = sf_check(build_info, bsObj, fast_flag, True)
+	if fversion == None:
+		return analyze_failed(name)
 	flink2 = ("https://sourceforge.mirrorservice.org"
 		"/n/ni/nitrogen-project/kenzo/kenzo_test/8.1/"
 		 + fversion)
 	build_info['flink'] = \
-		"# Sourceforge:\n\n    " + flink1 + \
+		"# Sourceforge:\n\n    " + build_info['flink'] + \
 		"\n\n    # Mirror for Sourceforge:\n\n    " + flink2
 	return out_put(fast_flag, name, fversion, build_info)
 
@@ -704,42 +634,13 @@ def nos_s(fast_flag, bs4_parser):
 	bsObj = get_bs(ual, bs4_parser)
 	if not bsObj:
 		return open_failed(name)
-	if fast_flag:
-		try:
-			nb = bsObj.find("channel").find_all("item")
-			nb1 = nb[0]; nb2 = nb[1]
-			if nb2.find("files:extra-info").get_text() != "text":
-				nb1, nb2 = nb2, nb1
-			build_info['update_log'] = nb2.find("guid").get_text()
-			flink1 = nb1.find("guid").get_text()
-			fversion = flink1.split("/")[-2]
-			build_info['fdate'] = nb1.find("pubdate").get_text()
-			build_info['fmd5'] = nb1.find("media:hash").get_text()
-		except:
-			return None
-	else:
-		try:
-			nb = bsObj.find("table",{"id":"files_list"})\
-				.find_all("tbody")[0].find_all("tr")[1]
-			nb2 = bsObj.find("table",{"id":"files_list"})\
-				.find_all("tbody")[0].find_all("tr")[0]
-			nb3 = json.loads(bsObj.find_all("script")[-1]\
-				.get_text().split(" = ",1)[-1].split(";",1)[0])
-			if nb["title"].split(".")[-1] != "zip":
-				nb, nb2 = nb2, nb
-			build_info['update_log'] = nb2.find("th").find("a")["href"]
-			fversion = nb["title"]
-			flink1 = nb.find("th").find("a")["href"]
-			build_info['fmd5'] = nb3[fversion]["md5"]
-			build_info['fsha1'] = nb3[fversion]["sha1"]
-			build_info['fdate'] = nb.find("td").find("abbr")["title"]
-			build_info['fsize'] = nb.find_all("td")[1].get_text()
-		except:
-			return analyze_failed(name)
+	fversion, build_info = sf_check(build_info, bsObj, fast_flag, True)
+	if fversion == None:
+		return analyze_failed(name)
 	flink2 = "https://sourceforge.mirrorservice.org" + \
 		"/n/ni/nitrogen-project/kenzo/kenzo_stable/" + fversion
 	build_info['flink'] = \
-		"# Sourceforge:\n\n    " + flink1 + \
+		"# Sourceforge:\n\n    " + build_info['flink'] + \
 		"\n\n    # Mirror for Sourceforge:\n\n    " + flink2
 	return out_put(fast_flag, name, fversion, build_info)
 
@@ -777,38 +678,9 @@ def pe_u1(fast_flag, bs4_parser):
 	bsObj = get_bs(ual, bs4_parser)
 	if not bsObj:
 		return open_failed(name)
-	if fast_flag:
-		try:
-			nb = bsObj.find("channel").find_all("item")
-			nb1 = nb[0]; nb2 = nb[1]
-			if nb2.find("files:extra-info").get_text() != "text":
-				nb1, nb2 = nb2, nb1
-			build_info['update_log'] = nb2.find("guid").get_text()
-			build_info['flink'] = nb1.find("guid").get_text()
-			fversion = build_info['flink'].split("/")[-2]
-			build_info['fdate'] = nb1.find("pubdate").get_text()
-			build_info['fmd5'] = nb1.find("media:hash").get_text()
-		except:
-			return None
-	else:
-		try:
-			nb = bsObj.find("table",{"id":"files_list"})\
-				.find_all("tbody")[0].find_all("tr")[1]
-			nb2 = bsObj.find("table",{"id":"files_list"})\
-				.find_all("tbody")[0].find_all("tr")[0]
-			nb3 = json.loads(bsObj.find_all("script")[-1]\
-				.get_text().split(" = ",1)[-1].split(";",1)[0])
-			if nb["title"].split(".")[-1] != "zip":
-				nb, nb2 = nb2, nb
-			build_info['update_log'] = nb2.find("th").find("a")["href"]
-			fversion = nb["title"]
-			build_info['fmd5'] = nb3[fversion]["md5"]
-			build_info['fsha1'] = nb3[fversion]["sha1"]
-			build_info['fdate'] = nb.find("td").find("abbr")["title"]
-			build_info['flink'] = nb.find("th").find("a")["href"]
-			build_info['fsize'] = nb.find_all("td")[1].get_text()
-		except:
-			return analyze_failed(name)
+	fversion, build_info = sf_check(build_info, bsObj, fast_flag, True)
+	if fversion == None:
+		return analyze_failed(name)
 	return out_put(fast_flag, name, fversion, build_info)
 
 def pe_u2b(fast_flag, bs4_parser):
@@ -820,38 +692,9 @@ def pe_u2b(fast_flag, bs4_parser):
 	bsObj = get_bs(ual, bs4_parser)
 	if not bsObj:
 		return open_failed(name)
-	if fast_flag:
-		try:
-			nb = bsObj.find("channel").find_all("item")
-			nb1 = nb[0]; nb2 = nb[1]
-			if nb2.find("files:extra-info").get_text() != "text":
-				nb1, nb2 = nb2, nb1
-			build_info['update_log'] = nb2.find("guid").get_text()
-			build_info['flink'] = nb1.find("guid").get_text()
-			fversion = build_info['flink'].split("/")[-2]
-			build_info['fdate'] = nb1.find("pubdate").get_text()
-			build_info['fmd5'] = nb1.find("media:hash").get_text()
-		except:
-			return None
-	else:
-		try:
-			nb = bsObj.find("table",{"id":"files_list"})\
-				.find_all("tbody")[0].find_all("tr")[1]
-			nb2 = bsObj.find("table",{"id":"files_list"})\
-				.find_all("tbody")[0].find_all("tr")[0]
-			nb3 = json.loads(bsObj.find_all("script")[-1]\
-				.get_text().split(" = ",1)[-1].split(";",1)[0])
-			if nb["title"].split(".")[-1] != "zip":
-				nb, nb2 = nb2, nb
-			build_info['update_log'] = nb2.find("th").find("a")["href"]
-			fversion = nb["title"]
-			build_info['fmd5'] = nb3[fversion]["md5"]
-			build_info['fsha1'] = nb3[fversion]["sha1"]
-			build_info['fdate'] = nb.find("td").find("abbr")["title"]
-			build_info['flink'] = nb.find("th").find("a")["href"]
-			build_info['fsize'] = nb.find_all("td")[1].get_text()
-		except:
-			return analyze_failed(name)
+	fversion, build_info = sf_check(build_info, bsObj, fast_flag, True)
+	if fversion == None:
+		return analyze_failed(name)
 	return out_put(fast_flag, name, fversion, build_info)
 
 def pe_u2s(fast_flag, bs4_parser):
@@ -863,38 +706,9 @@ def pe_u2s(fast_flag, bs4_parser):
 	bsObj = get_bs(ual, bs4_parser)
 	if not bsObj:
 		return open_failed(name)
-	if fast_flag:
-		try:
-			nb = bsObj.find("channel").find_all("item")
-			nb1 = nb[0]; nb2 = nb[1]
-			if nb2.find("files:extra-info").get_text() != "text":
-				nb1, nb2 = nb2, nb1
-			build_info['update_log'] = nb2.find("guid").get_text()
-			build_info['flink'] = nb1.find("guid").get_text()
-			fversion = build_info['flink'].split("/")[-2]
-			build_info['fdate'] = nb1.find("pubdate").get_text()
-			build_info['fmd5'] = nb1.find("media:hash").get_text()
-		except:
-			return None
-	else:
-		try:
-			nb = bsObj.find("table",{"id":"files_list"})\
-				.find_all("tbody")[0].find_all("tr")[1]
-			nb2 = bsObj.find("table",{"id":"files_list"})\
-				.find_all("tbody")[0].find_all("tr")[0]
-			nb3 = json.loads(bsObj.find_all("script")[-1]\
-				.get_text().split(" = ",1)[-1].split(";",1)[0])
-			if nb["title"].split(".")[-1] != "zip":
-				nb, nb2 = nb2, nb
-			build_info['update_log'] = nb2.find("th").find("a")["href"]
-			fversion = nb["title"]
-			build_info['fmd5'] = nb3[fversion]["md5"]
-			build_info['fsha1'] = nb3[fversion]["sha1"]
-			build_info['fdate'] = nb.find("td").find("abbr")["title"]
-			build_info['flink'] = nb.find("th").find("a")["href"]
-			build_info['fsize'] = nb.find_all("td")[1].get_text()
-		except:
-			return analyze_failed(name)
+	fversion, build_info = sf_check(build_info, bsObj, fast_flag, True)
+	if fversion == None:
+		return analyze_failed(name)
 	return out_put(fast_flag, name, fversion, build_info)
 
 def rr(fast_flag, bs4_parser):
@@ -906,32 +720,9 @@ def rr(fast_flag, bs4_parser):
 	bsObj = get_bs(ual, bs4_parser)
 	if not bsObj:
 		return open_failed(name)
-	if fast_flag:
-		try:
-			nb = bsObj.find("channel").find_all("item")
-			nb1 = nb[0]; nb2 = nb[1]
-			if nb2.find("files:extra-info").get_text() != "text":
-				nb1 = nb2
-			build_info['flink'] = nb1.find("guid").get_text()
-			fversion = build_info['flink'].split("/")[-2]
-			build_info['fdate'] = nb1.find("pubdate").get_text()
-			build_info['fmd5'] = nb1.find("media:hash").get_text()
-		except:
-			return None
-	else:
-		try:
-			nb = bsObj.find("table",{"id":"files_list"})\
-				.find_all("tbody")[0].find_all("tr")[1]
-			nb2 = json.loads(bsObj.find_all("script")[-1]\
-				.get_text().split(" = ",1)[-1].split(";",1)[0])
-			fversion = nb["title"]
-			build_info['fmd5'] = nb2[fversion]["md5"]
-			build_info['fsha1'] = nb2[fversion]["sha1"]
-			build_info['fdate'] = nb.find("td").find("abbr")["title"]
-			build_info['flink'] = nb.find("th").find("a")["href"]
-			build_info['fsize'] = nb.find_all("td")[1].get_text()
-		except:
-			return analyze_failed(name)
+	fversion, build_info = sf_check(build_info, bsObj, fast_flag)
+	if fversion == None:
+		return analyze_failed(name)
 	build_info['update_log'] = \
 		"https://sourceforge.net/projects/resurrectionremix" + \
 		"/files/kenzo/Changelog.txt/download"
